ea4f62ba589523c8c636272a2a475ff075ef0d7a
JOBS <80052912+jobswang0421@users.noreply.github.com>
2024-03-08 11:37:42 +0800

    WEEK3

--- guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
+++ guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
@@ -16,24 +16,41 @@ class MainActivity : AppCompatActivity() {
         val guess_button = findViewById<Button>(R.id.guess_button)
         val reset_button = findViewById<Button>(R.id.reset_button)
         val editText = findViewById<EditText>(R.id.editText)
-        var validate_num: Int=0
+        var validate_num: Int
         var big_num:Int=100
         var small_num:Int=0
         var secret: Int = Random().nextInt(100) + 1
-
+        var gs: Int
         guess_button.setOnClickListener {
             textView.text = editText.text
-
-            validate_num = editText.text.toString().toInt() - secret
+            if (editText.text.isNotEmpty()) {
+                gs = editText.text.toString().toInt()
+            }else{
+                gs =0
+            }
+            validate_num = gs - secret
             var ans_str: String = "CORRECT"
 
-            if (validate_num > 0) {
-                big_num=editText.text.toString().toInt()
+            if (gs == 0) {
+                ans_str ="PLEASE ENTER A NUMBER"
+            } else if (validate_num == 0) {
+                ans_str = "CORRECT"
+            }else if (validate_num > 0) {
+                if (big_num<gs){
+                    big_num=big_num
+                }else{
+                    big_num=gs
+                }
                 ans_str = "TOO BIG, RANGE: $small_num-$big_num"
-            } else if (validate_num < 0) {
-                small_num=editText.text.toString().toInt()
+            }else {
+                if (small_num>gs){
+                    small_num=small_num
+                }else{
+                    small_num=gs
+                }
                 ans_str = "TOO SMALL, RANGE: $small_num-$big_num"
             }
+
             textView.text = ans_str
 
         }
